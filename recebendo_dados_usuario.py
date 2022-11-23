de008e0963d11306a7eed399fe5d5f21b528c506
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-11-23 00:35:36 -0300

    Add files via upload
    Sessão 3, aula 12, recebendo dados do usuário.
    Até o momento do envio desse primeiro arquivo eu visualizei a aula até os 10min54seg. Logo, dou continuidade.

---
+++ recebendo_dados_usuario.py
@@ -0,0 +1,24 @@
+import os
+os.system('cls')
+
+##################################################
+#  Entrada 1
+print('\nExemplo 1...')
+print('Qual o número desse exemplo?')
+ex1 = input() #  Input é Entrada
+
+# Saida 1
+print('Esse exemplo é o número %s.\n' % ex1)
+
+##################################################
+#  Entrada 1.2
+print('\nVamos testar o exemplo 1.2...')
+print('Agora qual o número desse exemplo?')
+ex1_2 = input()
+print('Qual o seu nome?')
+nome = input()
+
+#  Saida 1.2
+print('Esse exemplo é o número %s. Obrigado pela participação %s.\n' % (ex1_2, nome))
+
+# CONTINUAR AULA...
