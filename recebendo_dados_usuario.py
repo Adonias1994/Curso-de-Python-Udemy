8a59a9ba281f5cb6f44d00bd59e467c541aef1b8
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-12-09 00:15:35 -0300

    Update recebendo_dados_usuario.py
    No próximo estudo realizarei novas atualizações.

--- recebendo_dados_usuario.py
+++ recebendo_dados_usuario.py
@@ -1,8 +1,7 @@
 import os
 os.system('cls')
 
-##################################################
-#  Entrada 1
+# Entrada 1
 print('\nExemplo 1...')
 print('Qual o número desse exemplo?')
 ex1 = input() #  Input é Entrada
@@ -10,15 +9,31 @@ ex1 = input() #  Input é Entrada
 # Saida 1
 print('Esse exemplo é o número %s.\n' % ex1)
 
-##################################################
-#  Entrada 1.2
-print('\nVamos testar o exemplo 1.2...')
+# Entrada 2
+print('\nVamos testar o exemplo 2...')
 print('Agora qual o número desse exemplo?')
-ex1_2 = input()
+ex2 = input()
 print('Qual o seu nome?')
 nome = input()
 
-#  Saida 1.2
-print('Esse exemplo é o número %s. Obrigado pela participação %s.\n' % (ex1_2, nome))
+# Saida 2
+print('Esse exemplo é o número %s. Obrigado pela participação %s.\n' % (ex2, nome))
 
-# CONTINUAR AULA...
+# Entrada 3
+print('\nVamos testar o exemplo 3...')
+print('Agora qual o número desse exemplo?')
+ex3 = input()
+
+# Saida 3
+print(f'Esse exemplo é o número {ex3}.\n')
+
+# Entrada 3.2
+noiva = input('\nQual o nome de sua noiva? ')
+idade_noiva = input('Qual a idade de sua noiva? ')
+
+# Saida 3.2
+print(f'{noiva}, noiva de Adonias, tem {idade_noiva} anos de idade.\n')
+
+print('Para um melhor entendimento, abra o código e leia ele com os comentários.\n')
+
+# CONTINUA...
