d48fdb99ff29a047fbf63ab93174e2e73dc6199c
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-12-18 20:27:46 -0300

    Add files via upload
    Sessão 4, Aula 15: Tipos numéricos.

---
+++ tipo_numerico.py
@@ -0,0 +1,27 @@
+import os
+os.system('cls')
+
+"""
+Tipo Numérico
+"""
+
+print('\nPara um melhor entendimento, abrir o código e ler os comentários.\n')
+
+ex1 = 5 + 2 # Adição ---> Realiza a soma
+print('5' + ' + ' + '2 =', ex1, '---> Adição ---> Realiza a soma.\n')
+ex2 = 5 - 2 # Subtração ---> Realiza a subtração
+print('5' + ' - ' + '2 =', ex2, '---> Subtração ---> Realiza a subtração.\n')
+ex3 = 5 * 2 # Multiplicação ---> Realiza a multiplicação
+print('5' + ' x ' + '2 =', ex3, '---> Multiplicação ---> Realiza a multiplicação.\n')
+ex4 = 5 / 2 # Divisão ---> Realiza a divisão
+print('5' + ' / ' + '2 =',ex4, '---> Divisão ---> Realiza a divisão.\n')
+ex5 = 5 // 2 # Divisão ---> Realiza a divisão em inteiro
+print('5' + ' // ' + '2 =',ex5, '---> Divisão ---> Realiza a divisão em inteiro.\n')
+ex6 = int(5 / 2) # Divisão ---> Realiza a divisão em inteiro através do casting
+print('5' + ' / ' + '2 =',ex6, '---> Divisão ---> Realiza a divisão em inteiro através do casting.\n')
+ex7 = 5 % 2 # Resto da divisão ---> Realiza a divisão e retorna o resto dela
+print('5' + ' % ' + '2 =',ex7, '---> Resto da divisão ---> Realiza a divisão e retorna o resto dela\n')
+ex8 = 5 ** 2 # Elevação ---> Um valor elevado a outro
+print('5' + ' ** ' + '2 =',ex8, '---> Elevação ---> Um valor elevado a outro.\n')
+
+# CONTINUA...
