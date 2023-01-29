e9348922c1892e775b1676e8a0d3119c7d51d945
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2023-01-29 16:45:21 -0300

    Add files via upload
    Continua...

---
+++ tipo_booleano.py
@@ -0,0 +1,20 @@
+"""
+Tipo Booleano
+
+Vem da Albebra Booleana que foi criada por George Boole
+
+Existem duas constantes, Verdadeiro ou Falso
+
+Em Python são representadas por:
+True -> Verdadeiro
+False -> Falso
+
+Obs:. Sempre com a iniciação maiúscula
+
+Errado:
+true, false
+
+Certo:
+True, False
+
+"""
