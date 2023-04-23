1066f65784a20b349013b8925266d80ceec6b54a
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2023-04-23 20:00:11 -0300

    Update tipo_booleano.py
    Continuidade da aula 17 da sessão 4.

--- tipo_booleano.py
+++ tipo_booleano.py
@@ -4,7 +4,7 @@ Tipo Booleano
 Vem da Albebra Booleana que foi criada por George Boole
 
 Existem duas constantes, Verdadeiro ou Falso
-
+ 
 Em Python são representadas por:
 True -> Verdadeiro
 False -> Falso
@@ -18,3 +18,45 @@ Certo:
 True, False
 
 """
+
+ativo = False
+print(ativo)
+
+"""
+Operações básicas:
+"""
+
+# Negação (not):
+
+"""
+Fazendo a negação, se o valor booleano for verdadeiro o resultado será falso,
+se for falso o resultado será verdadeiro, ou seja, sempre o contrário.
+"""
+print(not ativo)
+
+# Ou (or):
+
+"""
+É uma operação binária, ou seja, depende de dois valores. Ou um ou outro será verdadeiro.
+"""
+
+ou1 = True or True
+ou2 = True or False
+ou3 = False or True
+ou4 = False or False
+
+print(ou1, ou2, ou3, ou4)
+
+# E (and):
+
+"""
+Também é uma operação binária, ou seja, depende de dois valores.
+Ambos os dois valores devem ser verdadeiro.
+"""
+
+e1 = True and True
+e2 = True and False
+e3 = False and True
+e4 = False and False
+
+print(e1, e2, e3, e4)
