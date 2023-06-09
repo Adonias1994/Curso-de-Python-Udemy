b564d4ca5c49008f691c019922196e9c8cdcee6b
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2023-06-08 23:55:29 -0300

    Update 01.py
    Melhorias na resolução do desafio 1.

--- 01.py
+++ 01.py
@@ -6,5 +6,5 @@ Questão 1: Faça um programa que leia um número inteiro e o imprima
 import os
 os.system('cls')
 
-n1 = input()
-print(n1)
+n1 = int(input("Digite um número inteiro: ")) # n1 -> número 1
+print("O número inteiro digitado foi:", n1)
