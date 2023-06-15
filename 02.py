5514184c62a508dc431ad1c95c77bf18f62c2dab
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2023-06-15 01:39:48 -0300

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,10 @@
+'''
+Exercícios Python - Sessão 4
+Questão 2: Faça um programa que leia um número real e o imprima
+'''
+
+import os
+os.system('cls')
+
+n1 = float(input("Digite um número real: ")) # n1 -> número 1
+print("O número real digitado foi:", n1)
