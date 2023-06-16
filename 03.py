8fe9b85f03d8ee56d28339d851f1cec76610a7ae
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2023-06-16 01:52:36 -0300

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,9 @@
+'''
+Peça ao usuário para digitar três valores inteiros e imprima a soma deles
+'''
+
+v1 = int(input('Digite o primeiro valor: '))
+v2 = int(input('Digite o segundo valor: '))
+v3 = int(input('Digite o terceiro valor: '))
+
+print('\nA soma dos três valores é: ', v1 + v2 + v3, '\n')
