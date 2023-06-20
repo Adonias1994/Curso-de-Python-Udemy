d66564233f13806613159c646acb74df14ab4c69
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2023-06-19 23:24:43 -0300

    Add files via upload

---
+++ 04.py
@@ -0,0 +1,7 @@
+'''
+Leia um número real e imprima o resultado do quadrado desse número
+'''
+
+n_1 = float(input("\nInforme um número para ser calculado o quadrado dele: "))
+
+print("O quadrado do número informado é: ", n_1 * n_1, '\n')
