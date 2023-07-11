a4e7a923135f8551b42f6b34e3a61550f7ca796b
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2023-07-10 23:41:13 -0300

    Add files via upload

---
+++ 09.py
@@ -0,0 +1,11 @@
+'''
+Leia uma temperatura em graus Celsius e apresente-a convertira em Graus Kelvin.
+A fórmula de conversão é: K = C + 273.15, sendo C a temperatura em Celsius e K
+a temperatura em Kelvin.
+'''
+
+c = float(input("\nInforme a temperatura em graus Celsius: "))
+
+k = c + 273.15
+
+print("A temperatura informada convertida para graus Kelvin é", k, "\n")
